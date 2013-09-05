924eda5b78f717036b88af43858f0bb02a5c6869
Ed Dawson <edwin.dawson@gmail.com>
2013-09-06 01:07:33 +1000

    Commenting.

--- ledmorpher.py
+++ ledmorpher.py
@@ -117,7 +117,6 @@ def main():
         morphStartColour = []
         morphEndColour = []
         morphTransitionColour = []
-        morphRange = []
         redRange = 0
         redStepValue = 0
         redPolarity = 0
@@ -154,6 +153,7 @@ def main():
             #for i in morphEndColour: print i
             
             print "Morphing from", morphStartColour[0], "to", morphEndColour[0]
+            
             # Blanking old variables from the last colour set
             redStepValue = 0
             redRange = 0
@@ -217,17 +217,17 @@ def main():
                     morphTransitionColour[1] = (morphStartColour[1] + redStepValue)
                 if redPolarity == 0:
                     morphTransitionColour[1] = (morphStartColour[1] - redStepValue)
-                    print "Subtracted %d from %d" % (redStepValue, morphStartColour[1])
+                    #print "Subtracted %d from %d" % (redStepValue, morphStartColour[1])
                 if greenPolarity == 1:
                     morphTransitionColour[2] = (morphStartColour[2] + greenStepValue)
                 if greenPolarity == 0:
                     morphTransitionColour[2] = (morphStartColour[2] - greenStepValue)
-                    print "Subtracted %d from %d" % (greenStepValue, morphStartColour[2])
+                    #print "Subtracted %d from %d" % (greenStepValue, morphStartColour[2])
                 if bluePolarity == 1:
                     morphTransitionColour[3] = (morphStartColour[3] + blueStepValue)
                 if bluePolarity == 0:
                     morphTransitionColour[3] = (morphStartColour[3] - blueStepValue)
-                    print "Subtracted %d from %d" % (blueStepValue, morphStartColour[3])
+                    #print "Subtracted %d from %d" % (blueStepValue, morphStartColour[3])
                 print "%d, %d, %d" % (morphTransitionColour[1], morphTransitionColour[2], morphTransitionColour[3])
                 for each in range(32):
                     leds.setPixelColorRGB(pixel=each, red=morphTransitionColour[1], green=morphTransitionColour[2], blue=morphTransitionColour[3])
